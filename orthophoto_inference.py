1b4d65aac617269d564ccc4f5ae58746d63a7051
ibrahim-sall <ibhou2002@gmail.com>
2025-07-01 15:49:11 +0200

    refactor train.py

--- orthophoto_inference.py
+++ orthophoto_inference.py
@@ -266,5 +266,12 @@ Directory Structure:
 
 
 if __name__ == "__main__":
-    logger = setup_logger()
+    
+    logging.basicConfig(
+        level=logging.WARNING,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    logger = logging.getLogger(__name__)
+
     main()
